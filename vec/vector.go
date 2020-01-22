b2b3ff88ee94a9951674d66a6d44246a566ffe67
Kyriacos Souroullas <kyriacos.souroullas@gmail.com>
2020-01-21 19:54:23 -0800

    Fixed bug in vector

--- vec/vector.go
+++ vec/vector.go
@@ -11,5 +11,5 @@ func (v1 Vector2) Mul(c float32) Vector2 {
 
 // Add is equivalent to v3 := v1+v2
 func (v1 Vector2) Add(v2 Vector2) Vector2 {
-	return Vector2{v1.X + v2.Y, v1.Y + v2.Y}
+	return Vector2{v1.X + v2.X, v1.Y + v2.Y}
 }
